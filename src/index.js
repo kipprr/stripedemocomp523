eb0b2cbb9819338bfb9a0dd55d9f4f8d74bcb053
Kipp Williams <kipprw@live.unc.edu>
2020-11-01 15:14:59 -0500

    tried to use demo
    https://medium.com/@gordonnl/headless-stripe-payments-with-firebase-9b12639ea118

--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import firebase from 'firebase/app';
+import "firebase/database";
+
+// Set the configuration for your app
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: "AIzaSyCxUFp3R5sqvuEcwqZFn_OTGHxUSW_UG0M",
+  authDomain: "stripedemo-dc2b3.firebaseapp.com",
+  databaseURL: "https://stripedemo-dc2b3.firebaseio.com",
+  projectId: "stripedemo-dc2b3",
+  storageBucket: "stripedemo-dc2b3.appspot.com",
+  messagingSenderId: "746425677564",
+  appId: "1:746425677564:web:1a829a89848436cf20c979",
+  measurementId: "G-8JNWE64PXS"
+};
+
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
